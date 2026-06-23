55670c52dcee68e28f39e664a8fb16954a3a0e49
zaynabxon412 <bohodirovazaynabxon@gmail.com>
2026-06-23 09:13:57 +0500

    Hero

--- pense/src/Components/Hero/Hero.jsx
+++ pense/src/Components/Hero/Hero.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import './Hero.css'
 
 function Hero() {
   return (
-    <div>
-      <h1>M</h1>
+    <div className="Hero_body">
+      <div className='Hero_title'>
+      <h1>Track your Expenses & Save Money</h1>
+      <p>Helps you to organize your income and expenses</p>
+      <button>Try free demo </button>
+    </div>
+    <div className='Hero_img'>
+      <img src={'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTPj1-oQKIoiU-wHrFV-3rXlEmzSLR839XrflIyoClZMSBv_Jx7'} alt="" />
+    </div>
     </div>
   )
 }
