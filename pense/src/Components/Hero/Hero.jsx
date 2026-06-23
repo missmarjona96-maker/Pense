5a5c6b038325d90f461ab59d6d9b3a11255b17a8
missmarjona96-maker <missmarjona96@gmail.com>
2026-06-23 09:52:23 +0500

    Revert "Zaynabxon"

--- pense/src/Components/Hero/Hero.jsx
+++ pense/src/Components/Hero/Hero.jsx
@@ -1,19 +1,11 @@
-import './Hero.css'
+import React from 'react'
 
 function Hero() {
   return (
-    <div className="Hero_body">
-      <div className='Hero_title'>
-      <h1>Track your Expenses & Save Money</h1>
-      <p>Helps you to organize your income and expenses</p>
-      <button>Try free demo </button>
-    </div>
-    <div className='Hero_img'>
-      <img src={'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTPj1-oQKIoiU-wHrFV-3rXlEmzSLR839XrflIyoClZMSBv_Jx7'} alt="" />
-    </div>
+    <div>
+      <h1>M</h1>
     </div>
   )
-  
 }
 
 export default Hero
