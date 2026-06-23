aed9a66e29286df61500d8b50bc6993d67afd660
Miss.Marjona <missmarjona96gmail.com>
2026-06-23 09:07:07 +0500

    Figma design

--- pense/src/App.jsx
+++ pense/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Footer from './Components/Footer/Footer'
 
 function App() {
  
@@ -6,6 +7,7 @@ function App() {
   return (
    <div>
     <h1>pense</h1>
+    <Footer/>
    </div>
   )
 }
