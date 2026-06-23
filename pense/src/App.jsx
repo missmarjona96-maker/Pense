e7ea1752da7cb882208752053dd039a445498cfb
Durdona <makhmudovadurdona61@gmail.com>
2026-06-23 09:35:28 +0500

    Navbar

--- pense/src/App.jsx
+++ pense/src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
+import Navbar from './Components/Navbar/Navbar'
 
 function App() {
  
 
   return (
    <div>
-    <h1>pense</h1>
+    <Navbar/>
    </div>
   )
 }
--- pense/src/Components/Navbar/Navbar.jsx
+++ pense/src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import "./Navbar.css"
 
 function Navbar() {
   return (
     <div>
-      <h1>D</h1>
+      <div className='navbar'>
+        <h1 className='navbar--title'>pense</h1>
+      <div className='navbar_links'>
+        <a className='navbar_links--link' href="">Features</a>
+        <a className='navbar_links--link' href="">About us</a>
+        <a className='navbar_links--link' href="">Pricing</a>
+        <a className='navbar_links--link' href="">Feedback</a>
+      </div>
+      <div className='navbar_btn'>
+        <button className='navbar_btn--btn1'>Request a demo</button>
+      </div>
+      </div>
     </div>
   )
 }
